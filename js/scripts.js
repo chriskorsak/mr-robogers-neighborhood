42279a8cbc41620b7db9f7e033dfde5a54d87d0b
chriskorsak <chris@chriskorsak.com>
2020-05-16 15:14:57 -0700

    add business logic for spec: The program returns a range of numbers from 0 to the user's inputed number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 //business logic
-
+function mrRoboger(inputValue) {
+  var countUp = [];
+  for (let index = 0; index <= inputValue; index+=1) {
+    countUp.push(index);
+  }
+  return countUp;
+}
 
 
 
@@ -9,8 +15,9 @@
 $(document).ready(function() {
   $("form#mr-roboger").submit(function() {
     event.preventDefault();
-    var inputValue = $("#input1").val(); //get value from form input 1
-    $("span#message").text(inputValue); //return input 1 value to make sure form works
+    var inputValue = parseInt($("#input1").val()); //get value from form input 1
+    var output = mrRoboger(inputValue);
+    $("span#message").text(output);
     $("div#output").show();
     $("#input1").val(""); //clear out input 1 so user doesn't have to delete.
   });
