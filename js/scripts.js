a8cc4eab8e00a3fbd664456907abaf984afd9b2d
chriskorsak <chris@chriskorsak.com>
2020-05-16 15:56:43 -0700

    add user interface logic for spec: return a range of numbers from 0 to user input number, while replaceing 1,2,3, with text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //business logic
 function mrRoboger(inputValue) {
-  var countUp = [];
+  var numbers = [];
   for (let index = 0; index <= inputValue; index+=1) {
-    countUp.push(index);
+    numbers.push(index);
   }
-  return countUp;
+  return numbers;
 }
 
 
@@ -17,6 +17,18 @@ $(document).ready(function() {
     event.preventDefault();
     var inputValue = parseInt($("#input1").val()); //get value from form input 1
     var output = mrRoboger(inputValue);
+
+    if (output[3]) {
+      output[1] = "Beep!";
+      output[2] = "Boop!";
+      output[3] = "Won't you be my neighbor?"
+    } else if (output[2]) {
+      output[1] = "Beep!";
+      output[2] = "Boop!";
+    } else if (output[1]) {
+      output[1] = "Beep!"; 
+    }  
+
     $("span#message").text(output);
     $("div#output").show();
     $("#input1").val(""); //clear out input 1 so user doesn't have to delete.
