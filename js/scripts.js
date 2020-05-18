78135e4b243a61c47015ef99ac6f7b2866644e5f
chriskorsak <chris@chriskorsak.com>
2020-05-17 17:56:45 -0700

    refactor logic for testing if 1,2,or 3 in user input. now using a forEach loop and if else statements instead of repeating code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,25 @@
 //business logic
 function mrRobogerCounts(inputValue) { 
   var numbers = [];
-  for (let index = 0; index <= inputValue; index+=1) {
+  for (let index = 0; index <= inputValue; index += 1) {
     numbers.push(index);
   }
   return numbers;
 }
-function inputContains1(inputValue) {
+function inputContains(inputValue) {
+  var output;
   var userInputString = inputValue.toString(); 
-  var userInputArray = userInputString.split(''); // split user input to array
-  var contains1 = userInputArray.includes("1")
-  return contains1;
-}
-function inputContains2(inputValue) {
-  var userInputString = inputValue.toString(); 
-  var userInputArray = userInputString.split(''); // split user input to array
-  var contains2 = userInputArray.includes("2")
-  return contains2;
-}
-function inputContains3(inputValue) {
-  var userInputString = inputValue.toString(); 
-  var userInputArray = userInputString.split(''); // split user input to array
-  var contains3 = userInputArray.includes("3")
-  return contains3;
+  var numbers = userInputString.split(''); // split user input to array
+  numbers.forEach(function(number) {
+    if (number === "1") {
+    output = 1;
+    } else if (number === "2") {
+        output = 2;
+    } else if (number === "3") {
+        output = 3;
+    }
+  });
+  return output;
 }
 
 //user interface logic
@@ -34,37 +31,32 @@ $(document).ready(function() {
       $("span#message").text("You picked the magic number. Bravo!");
       $("span#message").addClass("magic-number");
     } else {
-      var output = mrRobogerCounts(inputValue); // function that counts from 0 to user input number
+        var output = mrRobogerCounts(inputValue); // function that counts from 0 to user input number
+        if (output[3]) {
+          output[1] = "Beep!";
+          output[2] = "Boop!";
+          output[3] = "Won't you be my neighbor?"
+        } else if (output[2]) {
+            output[1] = "Beep!";
+            output[2] = "Boop!";
+        } else if (output[1]) {
+            output[1] = "Beep!"; 
+        }
 
-      if (output[3]) {
-        output[1] = "Beep!";
-        output[2] = "Boop!";
-        output[3] = "Won't you be my neighbor?"
-      } else if (output[2]) {
-        output[1] = "Beep!";
-        output[2] = "Boop!";
-      } else if (output[1]) {
-        output[1] = "Beep!"; 
+        var contains = inputContains(inputValue); //function tests for 1,2, or 3 in user input number
+        if (contains === 1) {
+          output = "Beep!";
+        } else if (contains === 2) {
+            output = "Boop!";
+        } else if (contains === 3) {
+            output = "Won't you be my neighbor?";
+        }
+
+        $("span#message").removeClass("magic-number");
       } 
-      var contains1 = inputContains1(inputValue); //function tests for 1 in user input
-      if (contains1 === true) {
-        output = "Beep!"
-      }
-      var contains2 = inputContains2(inputValue); //function tests for 2 in user input
-      if (contains2 === true) {
-        output = "Boop!"
-      }
-      var contains3 = inputContains3(inputValue); //function tests for 3 in user input
-      if (contains3 === true) {
-        output = "Won't you be my neighbor?"
-      }
-
-      $("span#message").removeClass("magic-number");
-    } 
 
     $("span#message").text(output);
     $("div#output").show();
     $("#input1").val(""); //clear out input 1 so user doesn't have to delete.
   });
-
 });
