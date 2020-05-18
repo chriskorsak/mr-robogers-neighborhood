66e2aadf3fac6b243b4491800269f8dc274ce4ca
chriskorsak <chris@chriskorsak.com>
2020-05-17 19:22:29 -0700

    fix inputContains() function logic after code refactor.

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,16 @@ function inputContains(inputValue) {
   var output;
   var userInputString = inputValue.toString(); 
   var numbers = userInputString.split(''); // split user input to array
-  numbers.forEach(function(number) {
-    if (number === "1") {
+  var contains1 = numbers.includes("1");
+  var contains2 = numbers.includes("2");
+  var contains3 = numbers.includes("3");
+  if (contains3 === true) {
+    output = 3;
+  } else if (contains2 === true) {
+    output = 2;
+  } else if (contains1 === true) {
     output = 1;
-    } else if (number === "2") {
-        output = 2;
-    } else if (number === "3") {
-        output = 3;
-    }
-  });
+  }
   return output;
 }
 
@@ -28,8 +29,9 @@ $(document).ready(function() {
     event.preventDefault();
     var inputValue = parseInt($("#input1").val()); //gets value from form input 1
     if (inputValue === 12321) {
-      $("span#message").text("You picked the magic number. Bravo!");
-      $("span#message").addClass("magic-number");
+      $("span.message").text("You picked the magic number. Bravo!");
+      $("span.message").addClass("magic-number");
+      $("img.magic-photo").show();
     } else {
         var output = mrRobogerCounts(inputValue); // function that counts from 0 to user input number
         if (output[3]) {
@@ -51,11 +53,11 @@ $(document).ready(function() {
         } else if (contains === 3) {
             output = "Won't you be my neighbor?";
         }
-
-        $("span#message").removeClass("magic-number");
+        $("img.magic-photo").hide(); //only hides if magic number before
+        $("span.message").removeClass("magic-number"); //only remove if magic number before
       } 
 
-    $("span#message").text(output);
+    $("span.message").text(output);
     $("div#output").show();
     $("#input1").val(""); //clear out input 1 so user doesn't have to delete.
   });
