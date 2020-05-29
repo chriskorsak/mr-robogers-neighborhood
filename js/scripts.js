aedd4a8520122654fd62fcaad1daf338b0e2494f
chriskorsak <chris@chriskorsak.com>
2020-05-28 18:40:57 -0700

    add logic that replaces 1,2,3 with custom message for each number that includes these digits.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,20 @@
 function mrRobogerCounts(inputValue) { 
   var numbers = [];
   for (let index = 0; index <= inputValue; index += 1) {
-    numbers.push(index);
+    numbers.push(index.toString());
   }
-  return numbers;
+  message = numbers.map(function(number) {
+    if (number.includes("1")) {
+      return number = "beep";
+    } else if (number.includes("2")) {
+      return number = "boop";
+    } else if (number.includes("3")) {
+      return number = "won't you be my neighbor?";
+    } else {
+      return number;
+    }
+  });
+  return message;
 }
 
 //user interface logic
@@ -18,11 +29,9 @@ $(document).ready(function() {
       $("img.magic-photo").show();
     } else {
         var output = mrRobogerCounts(inputValue); // function that counts from 0 to user input number
-
         $("img.magic-photo").hide(); //only hides if magic number before
         $("span.message").removeClass("magic-number"); //only remove if magic number before
       } 
-
     $("span.message").text(output);
     $("div#output").show();
     $("#input1").val(""); //clear out input 1 so user doesn't have to delete.
